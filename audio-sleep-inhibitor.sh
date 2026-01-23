2c2473f8d8d1b20cabbffce6efe47ab91aa8e044
Sneaky Oxygen <azurmacjustin@gmail.com>
2026-01-23 23:19:58 +0800

    Add files via upload

---
+++ audio-sleep-inhibitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Save as ~/bin/audio-sleep-inhibitor.sh
+
+while true; do
+    # Check if any application is playing audio
+    if pactl list sinks | grep -q "State: RUNNING"; then
+        # Audio is playing, inhibit sleep
+        systemd-inhibit --what=sleep --why="Audio playback active" --mode=block sleep infinity &
+        INHIBIT_PID=$!
+        # Wait until audio stops
+        while pactl list sinks | grep -q "State: RUNNING"; do
+            sleep 5
+        done
+        # Audio stopped, kill the inhibitor
+        kill $INHIBIT_PID 2>/dev/null
+        wait $INHIBIT_PID 2>/dev/null
+    fi
+    sleep 5
+done
